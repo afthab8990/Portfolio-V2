6d6c9cbcab23a5550440b08fde678fe4075635f8
afthab8990 <a.t.bdc1pbvr@gmail.com>
2025-08-31 11:58:47 +0530

    first commit

---
+++ pages.js
@@ -0,0 +1,35 @@
+let load = document.querySelectorAll(".pager");
+let title = document.getElementById("title")
+
+
+
+
+function toSentenceCase(str) {
+    if (!str) return "";
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function loadPage() {
+    load.forEach((el) => {
+        el.addEventListener("click", (e) => {
+            let text = e.currentTarget.textContent.trim();
+            let setter_page = toSentenceCase(text);
+            console.log(setter_page);
+            title.innerHTML= `<p>${setter_page}</p`
+             title.style.animation = "none";
+            void title.offsetWidth; 
+            title.style.animation = "loader 2s ease-in-out";
+              setTimeout(() => {
+                 const targetElement = document.getElementById(setter_page.toLowerCase());
+                if (targetElement) {
+                    targetElement.scrollIntoView({ behavior: "smooth" }); 
+                }    
+            }, 1000); // 2000ms = 2 seconds
+           
+            
+        });
+    });
+}
+loadPage();
+
+
